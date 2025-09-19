df5808d199e9e37c59c034c92ca71ef094aa8be5
Носов Антон <antonnosov863@gmail.com>
2025-09-19 15:56:41 +0300

    Новый фаил

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, world 1'
+def index():
+    return render_template('Main.html')
+
+@app.route('/News')
+def News():
+    return render_template('News.html')
+
+@app.route('/News/Promo')
+def Promo():
+    return 'Акции лол'
+
+@app.route('/News/Announcements')
+def Announcements():
+    return 'Анонсы'
+
+@app.route('/Products')
+def Products():
+    return render_template('Product.html')
+
+@app.route('/Products/Accessories')
+def Accessories():
+    return 'Аксесуары'
+
+
+@app.route('/acc_user/<username>')
+def User_name(username):
+    return render_template('hello.html', name = username)
 
 if __name__ == '__main__': #Точка входа нашей программы 
-    print("Привет мир!") 
+    print("пошел нахуй со своми лабами") 
     app.run(debug=True)
